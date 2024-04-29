ce467157a75e7e67f1076f267b1291cb475cda9d
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-04-29 11:57:59 -0300

    feat: handleSignin

--- src/screens/sign-in/index.tsx
+++ src/screens/sign-in/index.tsx
@@ -1,16 +1,47 @@
+import { IOS_CLIENT_ID, WEB_CLIENT_ID } from '@env';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { useState } from 'react';
+import { Alert } from 'react-native';
+
 import { Container, Title, Slogan } from './styles';
 
 import { Button } from '@/components/Button';
 import backgroundImg from '@/shared/assets/background.png';
 
+GoogleSignin.configure({
+  scopes: ['email', 'profile'],
+  webClientId: WEB_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID,
+});
+
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function handleGoogleSignIn() {
+    try {
+      setIsAuthenticating(true);
+
+      const { idToken } = await GoogleSignin.signIn();
+      console.log('🚀 ~ handleGoogleSignIn ~ idToken:', idToken);
+
+      if (idToken) {
+      } else {
+        Alert.alert('Entrar', 'Não foi possível conectar-se a sua conta google.');
+        setIsAuthenticating(false);
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Entrar', 'Não foi possível conectar-se a sua conta google.');
+      setIsAuthenticating(false);
+    }
+  }
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
 
       <Slogan>Gestão de uso de veículos</Slogan>
 
-      <Button title="Entrar" />
+      <Button title="Entrar com Google" onPress={handleGoogleSignIn} />
     </Container>
   );
 }
