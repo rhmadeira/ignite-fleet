295aff19579d78e3848059d4a9882e657bae65d6
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-04-28 22:58:11 -0300

    feat: prebuild

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = ['react-native-paper/babel', "babel-plugin-styled-components"];
+  const plugins = [
+    'react-native-paper/babel',
+    'babel-plugin-styled-components',
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        allowUndefined: false,
+      },
+    ],
+  ];
 
   return {
     presets: ['babel-preset-expo'],
--- src/screens/sign-in/index.tsx
+++ src/screens/sign-in/index.tsx
@@ -1,5 +1,7 @@
 import { Container, Title, Slogan } from './styles';
-import backgroundImg from '../../assets/background.png';
+
+import { Button } from '@/components/Button';
+import backgroundImg from '@/shared/assets/background.png';
 
 export function SignIn() {
   return (
@@ -7,6 +9,8 @@ export function SignIn() {
       <Title>Ignite Fleet</Title>
 
       <Slogan>Gestão de uso de veículos</Slogan>
+
+      <Button title="Entrar" />
     </Container>
   );
 }
