d7fef64de9ee6458f4ab07ceaa90acc54efac6d4
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-05-04 00:36:59 -0300

    feat: logout.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { FontAwesome } from '@expo/vector-icons';
+import { useUser, useApp } from '@realm/react';
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
 
@@ -7,18 +8,28 @@ import { Container, Greeting, Message, Name, Picture } from './styles';
 import theme from '@/shared/theme';
 
 export default function HomeHeader() {
+  const user = useUser();
+  const app = useApp();
+
+  function handleLogout() {
+    app.currentUser?.logOut();
+  }
+
   return (
     <Container>
       <Picture
-        source={{ uri: 'https://github.com/rhmadeira.png' }}
+        source={{ uri: user.profile.pictureUrl }}
         placeholder="L184i9ofbHof00ayjsay~qj[ayj@"
       />
       <Greeting>
         <Message>Olá</Message>
 
-        <Name>Rafael</Name>
+        <Name>{user.profile.name}</Name>
       </Greeting>
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={handleLogout}
+        style={{ padding: 8, borderRadius: 8 }}
+        activeOpacity={0.8}>
         <FontAwesome name="power-off" size={32} color={theme.COLORS.GRAY_400} />
       </TouchableOpacity>
     </Container>
