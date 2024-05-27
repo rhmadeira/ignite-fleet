fb2e84703162d8d5a5571892a6c03a3a7fef7628
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-05-27 11:18:53 -0300

    feat: filter license-plate

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,21 +1,39 @@
 import { useNavigation } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import { Results } from 'realm';
 
 import { Container, Content } from './styles';
 
 import { CarStatus } from '@/components/CarStatus';
 import HomeHeader from '@/components/HomeHeader';
+import { useQuery } from '@/libs/realm';
+import { Historic } from '@/libs/realm/schemas/historic';
 
 export default function Home() {
   const { navigate } = useNavigation();
+  const [vehicleInUse, setVehicleInUse] = useState<Results<Historic> | []>([]);
+
+  const historic = useQuery(Historic);
 
   function handleRegisterMoviment() {
     navigate('departure');
   }
+
+  function fetchVehicle() {
+    const vehicleInUse = historic.filtered('status = "departure"');
+    console.log('🚀 ~ fetchVehicle ~ vehicleInUse:', vehicleInUse);
+
+    setVehicleInUse(vehicleInUse);
+  }
+
+  useEffect(() => {
+    fetchVehicle();
+  }, []);
   return (
     <Container>
       <HomeHeader />
       <Content>
-        <CarStatus licensePlate="XXX-1234" onPress={handleRegisterMoviment} />
+        <CarStatus licensePlate={vehicleInUse[0].license_plate} onPress={handleRegisterMoviment} />
       </Content>
     </Container>
   );
